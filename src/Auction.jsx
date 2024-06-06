e9d1031600a68997ad262c283ee0d456b6ff9e56
amithah <amithah.nithin@gmail.com>
2024-06-06 10:14:31 +0400

    ws to socket.io

--- src/Auction.jsx
+++ src/Auction.jsx
@@ -5,89 +5,70 @@ import { useNavigate, useParams } from "react-router-dom";
 import Timer from "./ui/Timer";
 import { addBid } from "./feature/bid/bidSlice";
 import Loader from "./ui/Loader";
+import { io } from "socket.io-client";
+import { useSocket } from "./hooks/useSocket";
 
 export function Auction() {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
 
   const { isLoading, auction } = useSelector((state) => state?.auctions);
-  const [ws, setWs] = useState(null);
   const [currentBid, setCurrentBid] = useState(""); // State to track the current bid value
   const [bidList, setBidList] = useState([]); // State to store the list of bids
+  const socket = useSocket(); // Custom hook or any other way you are managing the socket instance
 
   useEffect(() => {
     dispatch(fetchAuction(id));
-    const socket = new WebSocket(`wss://${import.meta.env.VITE_REACT_APP_URL}/${id}`);
-    setWs(socket);
 
-    socket.onopen = function () {
-      console.log("WebSocket connected");
-    };
+    const socket = io(import.meta.env.VITE_REACT_APP_URL, {
+      query: { roomId: id },
+    });
 
-    socket.onmessage = function (event) {
-      const data = event.data;
+    socket.on("connect", () => {
+      console.log("Socket.io connected");
+    });
 
-      if (typeof data === "string") {
-        // Try to parse JSON data
-        try {
-          const jsonData = JSON.parse(data);
-          if (jsonData.type === "previousBids") {
-            setBidList(jsonData.data.map((bid) => bid));
-          } else if (jsonData.type === "bid") {
-            setBidList((prevBidList) => [
-              ...prevBidList,
-              jsonData.value,
-            ]);
-          }
-        } catch (error) {
-          console.error("Error parsing JSON:", error);
-        }
-      } else if (data instanceof Blob) {
-        // Handle Blob data
-        const reader = new FileReader();
-        reader.onload = function () {
-          const convertedData = reader.result;
-          try {
-            const jsonData = JSON.parse(convertedData);
-            if (jsonData.type === "bid") {
-              setBidList((prevBidList) => [jsonData.value, ...prevBidList]);
-            }
-          } catch (error) {
-            console.error("Error parsing JSON:", error);
-            // Handle the error here
-          }
-          console.log("received (converted from Blob): ", convertedData);
-          // Handle the converted data here
-        };
-        reader.readAsText(data);
-      }
-    };
-    socket.onclose = function () {
-      console.log("WebSocket disconnected");
-    };
+    socket.on("previousBids", (data) => {
+      console.log(data)
+      setBidList(data?.map((bid) => bid));
+    });
+
+    socket.on("bid", (bid) => {
+      setBidList((prevBidList) => [...prevBidList, bid]);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket.io disconnected");
+    });
 
     return () => {
-      if (socket) {
-        socket.close();
-      }
+      socket.disconnect();
       dispatch(setAuction(null));
     };
   }, [id, dispatch]);
 
-  // Function to handle bid submission
   const submitBid = () => {
-    if (ws && currentBid !== "") {
-      const bidData = { type: "bid", value: { amount: currentBid,
-        auction: auction?._id,
-        addedBy: user,} };
-      ws.send(JSON.stringify(bidData));
+    if (socket && currentBid !== "") {
+      const bidData = {
+        type: "bid",
+        value: {
+          amount: currentBid,
+          auction: auction?._id,
+          addedBy: user,
+        },
+      };
+
+      // Emit the bidData using socket.io
+      socket.emit("message", bidData);
+
       const data = {
         amount: currentBid,
         auction: auction?._id,
         addedBy: user?._id,
       };
+
       dispatch(addBid(data));
       setCurrentBid(""); // Clear the input field after submitting the bid
     }
@@ -122,13 +103,13 @@ export function Auction() {
               <div className="grid grid-cols-2 gap-4 mt-2">
                 <div key={auction?._id} className="group relative w-1/2">
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                    {auction?.item?.images?.[0] &&
-                    <img
-                      src={`https://bidhub.s3.us-east-1.amazonaws.com/${auction?.item?.images?.[0]}`}
-                      alt={auction?.item?.name}
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
-}
+                    {auction?.item?.images?.[0] && (
+                      <img
+                        src={`https://bidhub.s3.us-east-1.amazonaws.com/${auction?.item?.images?.[0]}`}
+                        alt={auction?.item?.name}
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    )}
                   </div>
                   <div className="mt-4 flex justify-between">
                     <div>
@@ -147,7 +128,8 @@ export function Auction() {
                 </div>
 
                 <div className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
-                  {auction?.status !== "Finished" && auction?.createdBy?._id!==user?._id &&
+                  {auction?.status !== "Finished" &&
+                    auction?.createdBy?._id !== user?._id &&
                     auction?.status === "Ongoing" && (
                       <>
                         {/* Input field for bidding */}
@@ -164,19 +146,20 @@ export function Auction() {
                               className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                             />
                             <button
-                            disabled={!user}
+                              disabled={!user}
                               onClick={submitBid}
                               className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                             >
                               Submit
                             </button>
-                            {           !user&&    <button
-                        
-                              onClick={navigate("/")}
-                              className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            >
-                              Login
-                            </button>}
+                            {!user && (
+                              <button
+                                onClick={navigate("/")}
+                                className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                              >
+                                Login
+                              </button>
+                            )}
                           </div>
                         </div>
                       </>
@@ -184,7 +167,9 @@ export function Auction() {
                   {/* Display bid list */}
 
                   <div className="col-span-full mb-6">
-                    <h3 className="text-xl font-semibold mb-2">{bidList?.length===0?"No bids" :"Bid List"}</h3>
+                    <h3 className="text-xl font-semibold mb-2">
+                      {bidList?.length === 0 ? "No bids" : "Bid List"}
+                    </h3>
                     <ul className="divide-y divide-gray-200">
                       {bidList?.map((bid, index) => (
                         <li key={index} className="py-2">
---
+++ src/hooks/useSocket.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState, useContext, createContext } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(import.meta.env.VITE_REACT_APP_URL, {
+      query: { roomId: "your-auction-room-id" },
+    });
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
